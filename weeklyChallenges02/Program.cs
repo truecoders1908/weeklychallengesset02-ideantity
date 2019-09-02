6da6f9ce4e113455522e1de24ba375fcb5136028
Aziza Hurt <imazizahurt@gmail.com>
2019-09-02 17:33:12 -0500

    added second set of challenges

---
+++ weeklyChallenges02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace weeklyChallenges02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
